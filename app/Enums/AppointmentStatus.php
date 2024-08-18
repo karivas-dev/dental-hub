988fa6672fe884ef3212da8731ad02a2a97e12e0
Yosiak-alv <Josias_romero60@outlook.com>
2024-08-18 17:33:40 -0600

    Update AppointmentStatus.php
    ._.

--- app/Enums/AppointmentStatus.php
+++ app/Enums/AppointmentStatus.php
@@ -4,6 +4,8 @@ namespace App\Enums;
 
 enum AppointmentStatus: string
 {
+    use CasesAsOptions;
+    
     case Programada = 'Programada';
     case Reagendada = 'Reagendada';
     case Cancelada = 'Cancelada';
