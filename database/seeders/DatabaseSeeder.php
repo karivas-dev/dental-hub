b376d8942a5bb94c2b29b85bdc2f8962786864f4
Yosiak-alv <Josias_romero60@outlook.com>
2024-08-18 17:32:57 -0600

    Generated realistic seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Branch;
+use App\Models\Clinic;
+use App\Models\Role;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -14,10 +17,33 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        $admin = Role::create(['type' => 'Administración']);
+        $doctor = Role::create(['type' => 'Doctor']);
+        
+        User::factory()->recycle($admin)->create([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'admin' => true,
+        ]);
 
-        User::factory()->create([
+        $happyDentsMain = Branch::factory()->recycle(
+            Clinic::create(['name' => 'Happy Dents'])
+        )->create([
+            'name' => 'Happy Dents - Edificios Morados',
+            'main' => true,
+            'address' => 'Condominio Medicentro La Esperanza, Edificio "C" Local #211',
+            'phone' => '12345678',
+            'email' => 'happydents@email.com'
+        ]); 
+
+        User::factory()->recycle($admin)->recycle($happyDentsMain)->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        User::factory()->recycle($doctor)->recycle($happyDentsMain)->create([
+            'name' => 'Doctor User',
+            'email' => 'doctor@example.com'
+        ]); 
     }
 }
