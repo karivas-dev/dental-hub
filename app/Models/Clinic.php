e7cd3aceb66af0e247a802c646e03593dbdda049
bocchiko <luisbonilla0908@gmail.com>
2024-08-18 12:23:06 -0600

    Fixed Tenant Problems in User & Tenant Events

--- app/Models/Clinic.php
+++ app/Models/Clinic.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Clinic extends Model
@@ -33,4 +34,10 @@ class Clinic extends Model
     {
         return $this->hasMany(Branch::class);
     }
+
+    public function users(): HasManyThrough
+    {
+        return $this->hasManyThrough(User::class, Branch::class);
+    }
+
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -32,6 +32,7 @@ class User extends Authenticatable implements HasTenants
         'password',
         'role_id',
         'branch_id',
+        'admin',
     ];
 
     /**
