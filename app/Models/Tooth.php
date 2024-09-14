d33adf63a509984db858ab9f4400dc686e709d80
Yosiak-alv <Josias_romero60@outlook.com>
2024-09-14 16:44:58 -0600

    Created ToothRecords CRUD

--- app/Models/Tooth.php
+++ app/Models/Tooth.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Tooth extends Model
 {
@@ -26,4 +27,9 @@ class Tooth extends Model
     protected $casts = [
         'id' => 'integer',
     ];
+
+    public function toothRecords(): HasMany
+    {
+        return $this->hasMany(ToothRecord::class, );
+    }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Branch;
 use App\Models\Clinic;
 use App\Models\Role;
+use App\Models\Tooth;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -45,5 +46,64 @@ class DatabaseSeeder extends Seeder
             'name' => 'Doctor User',
             'email' => 'doctor@example.com'
         ]); 
+
+        $tooths = [
+            'Cordal Superior',
+            'Segundo Molar de Adulto Superior',
+            'Primer Molar de Adulto Superior',
+            'Segundo Premolar Superior',
+            'Primer Prmolar Superior',
+            'Canino Superior',
+            'Incisivo Lateral Superior',
+            'Incisivo Central Superior',
+            'Incisivo Central Superior',
+            'Incisivo Lateral Superior',
+            'Canino Superior',
+            'Primer Premolar Superior',
+            'Segundo Premolar Superior',
+            'Primer Molar de Adulto Superior',
+            'Segundo Molar de Adulto Superior',
+            'Cordal Sueprior',
+            'Cordal Inferior',
+            'Segundo Molar de Adulto Inferior',
+            'Primer Molar de Adulto Inferior',
+            'Segundo Premolar Inferior',
+            'Primer Premolar Inferior',
+            'Canino Inferior',
+            'Incisivo Lateral Inferior',
+            'Incisivo Central Inferior',
+            'Incisivo Central Inferior',
+            'Incisivo Lateral Inferior',
+            'Canino Inferior',
+            'Primer Premolar Inferior',
+            'Segundo Premolar Inferior',
+            'Primer Molar de Adulto Inferior',
+            'Segundo Molar de Adulto Inferior',
+            'Cordal Inferior',
+            'Segunda Molar Decidua Superior',
+            'Primera Molar Decidua Superior',
+            'Canino Deciduo Superior',
+            'Incisivo Lateral Deciduo Superior',
+            'Incisivo Central Deciduo Superior',
+            'Incisivo Central Deciduo Superior',
+            'Incisivo Lateral Deciduo Superior',
+            'Canino Deciduo Superior',
+            'Primera Molar Decidua Superior',
+            'Segunda Molar Decidua Superior',
+            'Segunda Molar Decidua Inferior',
+            'Primera Molar Decidua Inferior',
+            'Canino Deciduo Inferior',
+            'Incisivo Lateral Deciduo Inferior',
+            'Incisivo Central Deciduo Inferior',
+            'Incisivo Central Deciduo Inferior',
+            'Incisivo Lateral Deciduo Inferior',
+            'Canino Deciduo Inferior',
+            'Primera Molar Decidua Inferior',
+            'Segunda Molar Decidua Inferior'
+        ];
+
+        Tooth::factory(count($tooths))->sequence(fn($sequence) => [
+            'name' => $tooths[$sequence->index]
+        ])->create();
     }
 }
