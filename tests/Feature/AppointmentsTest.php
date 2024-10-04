4d9a1ddb774178c93ad56236dfcc35752da44fbf
Diego Gómez <72423267+Haruki1707@users.noreply.github.com>
2024-10-03 18:39:53 -0600

    Added AppointmentsTest

---
+++ tests/Feature/AppointmentsTest.php
@@ -0,0 +1,135 @@
+<?php
+
+use App\Filament\Resources\AppointmentResource;
+use App\Models\Appointment;
+
+use Filament\Actions\DeleteAction;
+
+use Filament\Tables\Actions\DeleteBulkAction;
+
+use function Pest\Laravel\assertDatabaseHas;
+use function Pest\Laravel\assertDatabaseMissing;
+use function Pest\Laravel\assertModelMissing;
+use function Pest\Laravel\assertSoftDeleted;
+use function Pest\Laravel\get;
+use function Pest\Livewire\livewire;
+
+it('can render index', function () {
+    get(AppointmentResource::getUrl('index'))->assertSuccessful();
+});
+
+it('can render create', function () {
+    get(AppointmentResource::getUrl('create'))->assertSuccessful();
+});
+
+it('can create appointment', function () {
+    $newAppointment = Appointment::factory()->make([
+        'date' => now()->addDay(),
+    ]);
+
+    livewire(AppointmentResource\Pages\CreateAppointment::class)
+        ->fillForm([
+            'date' => $newAppointment->date,
+            'details' => $newAppointment->details,
+            'status' => $newAppointment->status,
+            'amount' => $newAppointment->amount,
+            'user_id' => $newAppointment->user_id,
+            'patient_id' => $newAppointment->patient_id,
+        ])
+        ->call('create')
+        ->assertHasNoFormErrors();
+
+    assertDatabaseHas(Appointment::class, [
+        'date' => $newAppointment->date,
+        'status' => $newAppointment->status,
+        'amount' => $newAppointment->amount,
+        'user_id' => $newAppointment->user_id,
+        'patient_id' => $newAppointment->patient_id,
+    ]);
+});
+
+it('can validate input', function () {
+    livewire(AppointmentResource\Pages\CreateAppointment::class)
+        ->fillForm([
+            'date' => now()->subDay(),
+            'amount' => -1,
+            'user_id' => 0,
+            'patient_id' => 0,
+        ])
+        ->call('create')
+        ->assertHasFormErrors([
+            'date' => 'after',
+            'status' => 'required',
+            'amount' => 'min',
+            'user_id' => 'exists',
+            'patient_id' => 'exists',
+        ]);
+});
+
+// it('can render view', function () {
+//     get(AppointmentResource::getUrl('view', [Appointment::factory()->create()]))->assertSuccessful();
+// });
+
+it('can render edit', function () {
+    get(AppointmentResource::getUrl('edit', [Appointment::factory()->create()]))->assertSuccessful();
+});
+
+it('can retrieve data', function () {
+    $appointment = Appointment::factory()->create();
+
+    livewire(AppointmentResource\Pages\EditAppointment::class, ['record' => $appointment->getRouteKey()])
+        ->assertFormSet([
+            'date' => $appointment->date,
+            'details' => $appointment->details,
+            'status' => $appointment->status->value,
+            'amount' => $appointment->amount,
+            'user_id' => $appointment->user_id,
+            'patient_id' => $appointment->patient_id,
+        ]);
+});
+
+it('can update appointment', function () {
+    $appointment = Appointment::factory()->create();
+    $newAppointment = Appointment::factory()->make([
+        'date' => now()->addDay(),
+    ]);
+
+    livewire(AppointmentResource\Pages\EditAppointment::class, ['record' => $appointment->getRouteKey()])
+        ->fillForm([
+            'date' => $newAppointment->date,
+            'details' => $newAppointment->details,
+            'status' => $newAppointment->status,
+            'amount' => $newAppointment->amount,
+            'user_id' => $newAppointment->user_id,
+            'patient_id' => $newAppointment->patient_id,
+        ])
+        ->call('save')
+        ->assertHasNoFormErrors();
+
+    assertDatabaseHas(Appointment::class, [
+        'id' => $appointment->id,
+        'date' => $newAppointment->date,
+        'status' => $newAppointment->status,
+        'amount' => $newAppointment->amount,
+        'user_id' => $newAppointment->user_id,
+        'patient_id' => $newAppointment->patient_id,
+    ]);
+});
+
+it('can delete', function () {
+    $appointment = Appointment::factory()->create();
+
+    livewire(AppointmentResource\Pages\EditAppointment::class, ['record' => $appointment->getRouteKey()])
+        ->callAction(DeleteAction::class);
+
+    assertSoftDeleted($appointment);
+});
+
+it('can bulk delete', function () {
+    $appointments = Appointment::factory(2)->create();
+
+    livewire(AppointmentResource\Pages\ListAppointments::class)
+        ->callTableBulkAction(DeleteBulkAction::class, $appointments);
+
+    $appointments->each(fn ($appointment) => assertSoftDeleted($appointment));
+});
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,23 @@
 
 namespace Tests;
 
+use App\Models\User;
+use Filament\Facades\Filament;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->refreshDatabase();
+        $this->seed();
+        $user = User::where('admin', '=', 0)->first();
+        $this->actingAs($user);
+        Filament::setTenant($user->clinic);
+    }
 }
